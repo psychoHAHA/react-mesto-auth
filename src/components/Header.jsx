15736004decc2591f494c75cfff4098203e1df82
kirill_zernovv <80909413+psychoHAHA@users.noreply.github.com>
2023-10-15 21:42:20 +0500

    asdas

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link, BrowserRouter } from "react-router-dom";
 
 export default function Header() {
   return (
     <header className="header">
       <img src={require('../images/logo.svg').default} alt="лого замечательного сайта под названием Mesto, который я начинаю верстать 07.06.2023 в 17:01 по Екатеринбугскому времени. Делаю я это, совершенно не зная, как в дальнейшем мне предстоит писать JavaScript к этому проекту." className="header__logo" />
+      <BrowserRouter>
+        <Link className="header__link" to="/sign-up">
+          Регистрация
+        </Link>
+      </BrowserRouter>
     </header>
   )
 }
